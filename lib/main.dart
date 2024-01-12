d9452dbf44cc66fba307845ef17d4ee457c8564a
Simão Gomes <simaogomes764@gmail.com>
2024-01-12 16:34:40 +0000

    Dia 1 - Começo do projeto e já só falta colocar a API para os mapas. Até agora já tenho: 3 botões ligados às devidas páginas; a página do User faz conexão a um ficheiro JSON que está na minha GitHub a fazer de base de dados, com dados ficticios.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Barra com Ícones',
+      title: 'Flutter Mapa',
       home: MyHomePage(),
     );
   }
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  List<Map<String, dynamic>> usersData = [];
+
   @override
   void didChangeDependencies(){
     super.didChangeDependencies();
     print('PROFILE PAGE EXIBIDA');
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse('https://raw.githubusercontent.com/SGomes764/database/main/flutter_mapa/data.json')); // repositorio GitHub publico
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      setState(() {
+        usersData = List<Map<String, dynamic>>.from(data);
+      });
+    } else {
+      print('Erro ao carregar dados: ${response.statusCode}');
+    }
   }
 
   @override
@@ -21,11 +40,26 @@ class _ProfilePageState extends State<ProfilePage> {
         title: const Text('Profile Page'),
       ),
       body: Center(
-        child: Text(
-          'Conteúdo da Profile Page',
-          style: TextStyle(
-            fontSize: 20.0,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (final user in usersData)
+              Column(
+                children: [
+                  CircleAvatar(
+                  radius: 95.0,
+                  backgroundImage: CachedNetworkImageProvider(user["img"]),
+              ),
+              SizedBox(height: 100.0,),
+              Text(
+                'Nome: ${user["nome"]}\nSobrenome: ${user["sobrenome"]}\nUser: ${user["user"]}\nEmail: ${user["email"]}\nID: ${user["id"]}',
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
